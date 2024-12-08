919ab1c51ca56a860962cca8a4a8d81253935314
Ishan Aryan <singhishan67@gmail.com>
2024-12-08 19:59:25 +0530

    Move all buttons to a div Moved all buttons to a div to use Event Delegation.

--- script.js
+++ script.js
@@ -12,64 +12,60 @@ function getComputerChoice(){
 }
 
 function playRound(computerChoice,humanChoice){
-
     humanChoice=humanChoice.toUpperCase();
     
     if(humanChoice===computerChoice){
         return 0
     }else if(humanChoice==="ROCK"){
         if(computerChoice==="PAPER"){
-            computerScore++
-            return -1
+            computerScore++;
         }else{
-            humanScore++
-             return 1
+            humanScore++;
         }
     }else if(humanChoice==="PAPER"){
         if(computerChoice==="SCISSORS"){
-            computerScore++
-            return -1
+            computerScore++;
         }else{
-            humanScore
-             return 1
+            humanScore;
         }
     }else if(humanChoice==="SCISSORS"){
         if(computerChoice==="ROCK"){
-            computerScore++
-            return -1
+            computerScore++;
         }else{
-            humanScore++
-             return 1
+            humanScore++;
         }
+    }
+
+    if(computerScore===5){
+        return -5;
+    }
+    if(humanScore===5){
+        return 5;
     }
 }
 
 const rock=document.querySelector("#rock");
 const paper=document.querySelector("#paper");
 const scissors=document.querySelector("#scissors");
+const buttons=document.querySelector("#buttons");
 
-rock.addEventListener('click',()=>{
-    console.log("rock clicked");
+buttons.addEventListener('click',function(event){
+    console.log(event.target.id);
     let computerChoice=getComputerChoice();
-    playRound(computerChoice,"rock");
-    const div=document.querySelector("div");
-    div.textContent=`Player Score : ${humanScore} \t Computer Score : ${computerScore}`;
-});
-
-paper.addEventListener('click',()=>{
-    console.log("paper clicked");
-    let computerChoice=getComputerChoice();
-    playRound(computerChoice,"paper");
-    const div=document.querySelector("div");
-    div.textContent=`Player Score : ${humanScore} \t Computer Score : ${computerScore}`;
-});
-
-scissors.addEventListener('click',()=>{
-    console.log("scissors clicked");
-    let computerChoice=getComputerChoice();
-    playRound(computerChoice,"scissors");
-    const div=document.querySelector("div");
-    div.textContent=`Player Score : ${humanScore} \t Computer Score : ${computerScore}`;
-});
-
-
+    console.log(`Computer Choice : ${computerChoice}`)
+    let score=playRound(computerChoice,event.target.id);
+    const div=document.querySelector("#container");
+    if(score===5){
+        div.textContent="CONGRATULATIONS! You WIN :)";
+        div.textContent+="Final Scores :";
+        div.textContent+=`Player Score : ${humanScore} Computer Score : ${computerScore}`
+        humanScore=0;computerScore=0;
+    }else if(score===-5){
+        div.textContent="OOPS! You LOSE :(";
+        div.textContent+="Final Scores :";
+        div.textContent+=`Player Score : ${humanScore} Computer Score : ${computerScore}`
+        humanScore=0;computerScore=0;
+    }else{
+        div.textContent=`Player Score : ${humanScore} Computer Score : ${computerScore}`;
+    }
+})
